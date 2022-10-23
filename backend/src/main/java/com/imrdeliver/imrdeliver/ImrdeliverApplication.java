775b0dfcbfbc3570253a8c32450d7d07aac8152c
Suleimane <manuel.sbmn@gmail.com>
2022-10-22 23:07:13 -0300

    Configuração inicial

---
+++ backend/src/main/java/com/imrdeliver/imrdeliver/ImrdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.imrdeliver.imrdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImrdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImrdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/imrdeliver/imrdeliver/ImrdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imrdeliver.imrdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImrdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
